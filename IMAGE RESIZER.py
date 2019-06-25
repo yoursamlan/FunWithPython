386c656c77cb9ff1cb2d27c66facc38b7a17c4a8
Amlan Saha Kundu <skamlan99@gmail.com>
2019-06-25 17:12:15 +0530

    Create IMAGE RESIZER.py
    Prerequisites:
    
    Please make sure that you have installed the following modules.
    
    1. imageio
    2. scikit-image
    3. pillow
    4. numpy
    
    You can use "sudo pip install (MODULE NAME)"  or   "pip install (MODULE NAME)" in your terminal/command prompt to install the modules.

---
+++ IMAGE RESIZER.py
@@ -0,0 +1,54 @@
+#IMAGE RESIZER
+#version 0.1
+#by Amlan Saha Kundu
+
+#FunWithPython --- Day 0, Hour 1.
+
+#==========================================================================================================================================================================================
+
+
+from scipy.misc import *
+from imageio import *
+import skimage
+from skimage import *
+
+#Welcome User :)
+print('Input your name :')
+user = input()
+print('Hi '+ user + ' ! \n')
+
+#input the file location
+
+print('Please ENTER the input file location below')
+print('Remember USE FORWARD SLASH (/) instead of backslash (\\) while entering the file location.')
+print('eg: C:/Users/ABC/Pictures/xyz.jpg')
+print('Input your image location here :')
+loc = input()
+
+#reading the image...
+
+img = imread(str(loc))
+print (img.dtype, img.shape)
+
+#Enter parameter:
+
+print('Enter Height (px):')
+h = input()
+print('Enter Width (px):')
+w = input()
+
+#Enter output location:
+print('Please ENTER the output file location along its name below')
+print('Remember USE FORWARD SLASH (/) instead of backslash (\\) while entering the file location.')
+print('eg: C:/Users/ABC/Pictures/xyz_resized.jpg')
+print('Enter your Output image location here :')
+loco = input()
+
+#Resizing the image...
+res_img = skimage.transform.resize(img, (int(h),int(w)))
+
+#Save the output file:
+imsave(str(loco),res_img)
+
+
+
